1101efef0de1c4bb16d5223bfda88b75612bee7c
Hilder Arrieta <hbarrieta25@gmail.com>
2021-08-29 13:58:58 -0500

    Initial configuration

---
+++ src/test/java/com/hb/enterprise/sa/SaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hb.enterprise.sa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
